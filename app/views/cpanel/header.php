ea9b81b91b37987255408a2a1310d6fbb8243e2b
lduchieu512 <lduchieu512@gmail.com>
2023-11-25 13:55:16 +0700

    PC-test

--- app/views/cpanel/header.php
+++ app/views/cpanel/header.php
@@ -8,21 +8,13 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
  	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/public/template/css/style.css">
 	<title>Admin</title>
-
-<!--     <script>
-        $(function() {
-            $(document).ready(function() {
-                $('#example').DataTable();
-            });
-        });
-    </script> -->
-    
-
-	
 	<style type="text/css">
 		.navbar-nav>li>a {
     		font-size: 18px;
 		}
+        tr>th,tr>td {
+            font-size: 18px;
+        }
 	</style>
 
 </head>
