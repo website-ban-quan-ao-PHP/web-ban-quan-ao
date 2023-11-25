05b00ef4e5d0712a90a054e89c09bb66da192b47
lduchieu512 <lduchieu512@gmail.com>
2023-11-25 13:09:29 +0700

    PC-test

--- app/views/cpanel/header.php
+++ app/views/cpanel/header.php
@@ -9,23 +9,14 @@
  	<link rel="stylesheet" type="text/css" href="<?php echo BASE_URL ?>/public/template/css/style.css">
 	<title>Admin</title>
 
-
-	<!--   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.min.js" ></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" ></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js" ></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js" ></script> -->
-
-    <script>
+<!--     <script>
         $(function() {
             $(document).ready(function() {
                 $('#example').DataTable();
             });
         });
-    </script>
+    </script> -->
+    
 
 	
 	<style type="text/css">
